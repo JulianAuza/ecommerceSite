d4bac47e429d82287e7b90f6ab2aec9ad749f580
JulianAuza <julianauza@gmail.com>
2019-08-12 02:38:22 -0500

    added shirt color clicking

--- scripts.js
+++ scripts.js
@@ -4,6 +4,14 @@ $('.product-tile').click(function(){
  
 });
 
+function updateImage(filename){
+    console.log(filename)
+    var newFile= "assets/"+ filename + ".svg"
+    console.log(newFile)
+    $('.main-image').attr("src",newFile);
+    $('.view .color-tshirt-image').attr("src",newFile);
+};
+
 $('nav').click(function(event){
     var id = event.target.id
     $(".nav").removeClass('active')
@@ -11,10 +19,12 @@ $('nav').click(function(event){
 });
 
 $('.customize-tile').click(function(event){
+    var id = event.target.id
     if($( this ).hasClass( "size" )){
         $('.customize-tile.size').removeClass('active')
         $(this).addClass('active')
     }if($( this ).hasClass( "color" )){
+        updateImage($(this).attr('id'))
         $('.customize-tile.color').removeClass('active')
         $(this).addClass('active')
     }if($(this).hasClass('view')){
